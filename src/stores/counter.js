b4988dbce387c0ded41e068d7072ed8817657548
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2024-05-22 06:15:04 +0000

    chore: learn how stores and ref work

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -4,7 +4,9 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
+  
   function increment() {
+    console.log('hello');
     count.value++
   }
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,32 @@
+<script setup>
+  import { ref } from 'vue'
+  import { useCounterStore } from '@/stores/counter'
+
+  const count = useCounterStore();
+
+  const dialog = ref(null);
+
+  function clickMe() {
+    dialog.value.showModal();
+    count.increment();
+  }
+
+  function closeMe() {
+    dialog.value.close();
+  }
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <button @click="clickMe">Click me</button>
   </div>
+
+  <dialog ref="dialog">
+    <button @click="closeMe">Close Me</button>
+    Hello {{ count.count }}
+  </dialog>
 </template>
 
 <style>
